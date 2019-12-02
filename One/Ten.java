3dbc3a94a0da794787afd4fc7115247d93c963e7
vinosh kumar <vinosh kumar@LAPTOP-3MQPEHVO>
2019-12-02 16:19:02 +0530

    added class

---
+++ One/Ten.java
@@ -0,0 +1,17 @@
+
+public class Ten {
+	
+	int a=2;
+	int b=0;
+
+	public static void main(String[] args) {
+
+		try {
+			
+			 c=a/b;
+		}catch(ArithmeticException e)
+		System.err.println("exception occured:"+c);
+		
+	}
+
+}
